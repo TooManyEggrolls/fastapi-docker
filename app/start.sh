d6a24ebfa2c317bc06e0de410eb9d28ee5af669f
Alex Hosch <aphosch@gmail.com>
2021-11-13 21:02:37 -0600

    partial possible lint fix

--- app/start.sh
+++ app/start.sh
@@ -25,6 +25,7 @@ PRE_START_PATH=${PRE_START_PATH:-/app/prestart.sh}
 echo "Checking for script in $PRE_START_PATH"
 if [ -f $PRE_START_PATH ] ; then
     echo "Running script $PRE_START_PATH"
+    # shellcheck source=/dev/null
     . "$PRE_START_PATH"
 else 
     echo "There is no script $PRE_START_PATH"
